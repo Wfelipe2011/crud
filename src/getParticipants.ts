f8da902b3871d2a3d729648fcb703e7e4c60c0d7
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-06-04 18:29:05 -0300

    add promise all

--- src/getParticipants.ts
+++ src/getParticipants.ts
@@ -5,38 +5,39 @@ export const getParticipants = async (req: Request, res: Response) => {
   try {
     const queryParams = req.query as { filter: string; page: string };
     const page = parseInt(queryParams.page || "1") - 1;
-    const groups = await prisma.groups.findMany();
 
-    const participants = await prisma.participants.findMany({
-      where: {
-        ...(queryParams.filter && {
-          computed: {
-            contains: queryParams.filter.toLowerCase(),
-            mode: "insensitive",
-          },
-        }),
-      },
-      include: {
-        ParticipantsGroup: {
-          include: {
-            group: true,
+    const [groups, participants, count] = await Promise.all([
+      prisma.groups.findMany(),
+      prisma.participants.findMany({
+        where: {
+          ...(queryParams.filter && {
+            computed: {
+              contains: queryParams.filter.toLowerCase(),
+              mode: "insensitive",
+            },
+          }),
+        },
+        include: {
+          ParticipantsGroup: {
+            include: {
+              group: true,
+            },
           },
         },
-      },
-      skip: page * 10,
-      take: 10,
-    });
-
-    const count = await prisma.participants.count({
-      where: {
-        ...(queryParams.filter && {
-          computed: {
-            contains: queryParams.filter.toLowerCase(),
-            mode: "insensitive",
-          },
-        }),
-      },
-    });
+        skip: page * 10,
+        take: 10,
+      }),
+      prisma.participants.count({
+        where: {
+          ...(queryParams.filter && {
+            computed: {
+              contains: queryParams.filter.toLowerCase(),
+              mode: "insensitive",
+            },
+          }),
+        },
+      }),
+    ]);
 
     const pages = {
       total: Math.ceil(count / 10),
